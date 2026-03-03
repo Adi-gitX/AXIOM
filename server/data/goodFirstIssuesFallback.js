eb22e5049971a880ae119344f78026fdcecfe7f8
adigitX <adityakammati.workspace@gmail.com>
2026-03-04 03:55:04 +0530

    Create good first issues fallback

---
+++ server/data/goodFirstIssuesFallback.js
@@ -0,0 +1,44 @@
+export const CURATED_GOOD_FIRST_ISSUES = [
+    {
+        repo_full_name: 'freeCodeCamp/freeCodeCamp',
+        issue_number: 54773,
+        title: 'Fix typo in responsive web design docs',
+        html_url: 'https://github.com/freeCodeCamp/freeCodeCamp/issues/54773',
+        labels: ['good first issue', 'documentation'],
+        language: 'JavaScript',
+    },
+    {
+        repo_full_name: 'microsoft/vscode',
+        issue_number: 228721,
+        title: 'Improve welcome page keyboard accessibility',
+        html_url: 'https://github.com/microsoft/vscode/issues/228721',
+        labels: ['good first issue', 'accessibility'],
+        language: 'TypeScript',
+    },
+    {
+        repo_full_name: 'vercel/next.js',
+        issue_number: 70381,
+        title: 'Update docs example for app router metadata',
+        html_url: 'https://github.com/vercel/next.js/issues/70381',
+        labels: ['good first issue', 'documentation'],
+        language: 'TypeScript',
+    },
+    {
+        repo_full_name: 'nodejs/node',
+        issue_number: 53245,
+        title: 'Improve fs module examples',
+        html_url: 'https://github.com/nodejs/node/issues/53245',
+        labels: ['good first issue', 'doc'],
+        language: 'JavaScript',
+    },
+    {
+        repo_full_name: 'kubernetes/website',
+        issue_number: 47211,
+        title: 'Fix broken links in setup docs',
+        html_url: 'https://github.com/kubernetes/website/issues/47211',
+        labels: ['good first issue', 'documentation'],
+        language: 'Go',
+    },
+];
+
+export default CURATED_GOOD_FIRST_ISSUES;
