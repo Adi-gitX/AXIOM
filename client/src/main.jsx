a024cefe96a9c5cbcd9ce068ee55e591d31cc0fd
Adithya Kammati <kammati.a23csai@nst.rishihood.edu.in>
2026-05-01 01:36:10 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from './components/theme-provider'
+import { initSentry } from './lib/sentry'
+
+initSentry();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <ThemeProvider defaultTheme="dark" storageKey="axiom-theme">
+        <ThemeProvider defaultTheme="light" storageKey="axiom-theme-v2">
             <App />
         </ThemeProvider>
     </React.StrictMode>,
