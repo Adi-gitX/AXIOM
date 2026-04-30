b380a8984c5295f79b5b6870e47c116b8d59697b
Adithya Kammati <kammati.a23csai@nst.rishihood.edu.in>
2026-05-01 01:36:10 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -109,9 +109,17 @@ export default defineConfig({
         },
     },
     server: {
+        host: '0.0.0.0',
+        port: 3000,
+        strictPort: true,
+        allowedHosts: true,
+        hmr: {
+            clientPort: 443,
+            protocol: 'wss',
+        },
         proxy: {
             '/api': {
-                target: 'http://localhost:3000',
+                target: 'http://localhost:8001',
                 changeOrigin: true,
                 secure: false,
             }
